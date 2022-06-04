2dee5decbe6d2696c1307a6191ea568fb60c9b6b
AnhNSQ <nguye.sy.quang.anh@sun-asterisk.com>
2022-06-04 16:58:59 +0700

    Initial commit

---
+++ src/main/java/com/viettel/traffic_lab/TrafficLabApplication.java
@@ -0,0 +1,13 @@
+package com.viettel.traffic_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viettel/traffic_lab/TrafficLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viettel.traffic_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
